46c81c7386395bc2e637b7bc4adf4b8f0a3264ef
NomikLee <Nomiklee.icloud.com>
2024-01-15 08:41:30 +0800

    Initial Commit

---
+++ nomikStock/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  nomikStock
+//
+//  Created by Pinocchio on 2024/1/15.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
